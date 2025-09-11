5d38ad7e664f467c4f5b73eeadc067e84297c5d5
sheeedplabon <you@example.com>
2025-09-11 12:40:02 +0600

    update store

--- 2.React-redux-simple-todo/src/store.js
+++ 2.React-redux-simple-todo/src/store.js
@@ -1,55 +1,55 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const loadState = () => {
-    try {
-        const alltodo = localStorage.getItem("todos");
-        return alltodo ? JSON.parse(alltodo) : [];
-    } catch (err) {
-        console.error("Could not load state", err);
-        return [];
-    }
+  try {
+    const alltodo = localStorage.getItem("todos");
+    return alltodo ? JSON.parse(alltodo) : [];
+  } catch (err) {
+    console.error("Could not load state", err);
+    return [];
+  }
 };
 
 const saveState = (state) => {
-    try {
-        const alltodo = JSON.stringify(state);
-        localStorage.setItem("todos", alltodo);
-    } catch (err) {
-        console.error("Could not save state", err);
-    }
+  try {
+    const alltodo = JSON.stringify(state);
+    localStorage.setItem("todos", alltodo);
+  } catch (err) {
+    console.error("Could not save state", err);
+  }
 };
 const todoSlice = createSlice({
-    name: "todos",
-    initialState: loadState(),
-    reducers: {
-        addtodo: (state, action) => {
-            state.push({ id: Date.now(), text: action.payload, completed: false })
-        },
-        toggletodo: (state, action) => {
-            const todo = state.find((todo) => todo.id === action.payload)
-            if (todo) todo.completed = !todo.completed
-        },
-        deletetodo:(state,action)=>{
-            return state.filter((todo)=>todo.id!==action.payload)
-        },
-        updatetodo:(state,action)=>{
-            const {id,newtext}=action.payload
-            const todo=state.find((todo)=>todo.id===id)
-            if(todo)todo.text=newtext
+  name: "todos",
+  initialState: loadState(),
+  reducers: {
+    addtodo: (state, action) => {
+      state.push({ id: Date.now(), text: action.payload, completed: false });
+    },
+    toggletodo: (state, action) => {
+      const todo = state.find((todo) => todo.id === action.payload);
+      if (todo) todo.completed = !todo.completed;
+    },
+    deletetodo: (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload);
+    },
+    updatetodo: (state, action) => {
+      const { id, newText } = action.payload; 
+      const todo = state.find((todo) => todo.id === id);
+      if (todo) todo.text = newText;
+    },
+  },
+});
 
-        }
-    }
-})
+export const { addtodo, toggletodo, deletetodo, updatetodo } =
+  todoSlice.actions;
 
-export const {addtodo,toggletodo,deletetodo,updatetodo}=todoSlice.actions
+const store = configureStore({
+  reducer: {
+    todos: todoSlice.reducer,
+  },
+});
 
-const store=configureStore({
-    reducer:{
-        todos: todoSlice.reducer
-    }
-})
-
-store.subscribe(()=>{
-    saveState(store.getState().todos)
-})
-export default store
+store.subscribe(() => {
+  saveState(store.getState().todos);
+});
+export default store;
